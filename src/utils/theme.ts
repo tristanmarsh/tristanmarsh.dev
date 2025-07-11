4d8cfd1e4ea3f4113975c10a67700080f990761b
Tristan Marsh <tristan.marsh@canva.com>
2025-07-11 16:14:46 +1000

    add command bar

---
+++ src/utils/theme.ts
@@ -0,0 +1,7 @@
+export function setDarkTheme() {
+  document.documentElement.classList.add('theme-dark')
+}
+
+export function setLightTheme() {
+  document.documentElement.classList.remove('theme-dark')
+}
